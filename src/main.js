0496b2a752b3c2a5028a3a162498090f7b659d4f
k0f1sh <2574472+k0f1sh@users.noreply.github.com>
2024-12-12 01:59:26 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import './style.css'
 
+let timeOutId = null;
+
 //document.querySelector('#app').innerHTML
 document.getElementById('shred').addEventListener('click', () => {
   const input = document.getElementById('moyamoya').value;
   const shredContainer = document.getElementById('shred-container');
 
+
   // Clear previous content
   shredContainer.innerHTML = '';
+
+  const box = document.createElement('div');
+  box.className = 'shred-text-box';
+  shredContainer.appendChild(box);
 
   // Create shred animation for each character
   input.split('\n').forEach(line => {
@@ -17,9 +24,18 @@ document.getElementById('shred').addEventListener('click', () => {
       span.className = 'shred-text';
       shredLine.appendChild(span);
     })
-    shredContainer.appendChild(shredLine);
+    box.appendChild(shredLine);
   });
 
   // Clear input field
   document.getElementById('moyamoya').value = '';
+
+
+  // Clear after animation ends
+  timeOutId = setTimeout(() => {
+    if (timeOutId) {
+      clearTimeout(timeOutId);
+    }
+    shredContainer.innerHTML = '';
+  }, 10500);
 });
